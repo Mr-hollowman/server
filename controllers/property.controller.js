fa619fa4451b46b95af0467dd569ed89b05de038
mr-hollowman <selvamani825@gmail.com>
2023-02-16 22:29:30 +0530

    create property added

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -7,41 +7,62 @@ dotenv.config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
-const getAllProperties = async (req, res)=>{
-    
-}
-
-const getPropertyDetail = async (req, res)=>{
-    
-}
-
-const createProperty = async (req, res)=>{
-    const {title, description, propertyType, location, price, photo, email } = req.body;
-
-    //start a new session
-    const session = await mongoose.startSession();
-    session.startTransaction();
-
-    const user = await User.findOne({email}).session(session)
-
-    if(!user) throw new Error("User not found")
-
-
+const getAllProperties = async (req, res) => {
 
 }
 
-const updateProperty = async (req, res)=>{
-    
+const getPropertyDetail = async (req, res) => {
+
 }
 
-const deleteProperty = async (req, res)=>{
-    
+const createProperty = async (req, res) => {
+
+    try {
+        const { title, description, propertyType, location, price, photo, email } = req.body;
+
+        //start a new session
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        const user = await User.findOne({ email }).session(session)
+
+        if (!user) throw new Error("User not found")
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newProperty = await Property.create({
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo: photoUrl,
+            creator: user._id
+        })
+
+        user.allProperties.push(newProperty._id);
+        await user.save({ session });
+
+        await session.commitTransaction();
+        res.status(200).json({ message: "property added successfully" })
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 }
 
-export{
+const updateProperty = async (req, res) => {
+
+}
+
+const deleteProperty = async (req, res) => {
+
+}
+
+export {
     getAllProperties,
     getPropertyDetail,
     createProperty,
