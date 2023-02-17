d3c6e4226bd749eca1b7e87517cf66feef8f732a
mr-hollowman <selvamani825@gmail.com>
2023-02-17 10:40:05 +0530

    get all property added

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -12,7 +12,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 const getAllProperties = async (req, res) => {
+    try {
+        const properties = await Property.find({}).limit(req.query._end);
+        res.status(200).json(properties)
 
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 }
 
 const getPropertyDetail = async (req, res) => {
@@ -51,7 +57,7 @@ const createProperty = async (req, res) => {
         res.status(200).json({ message: "property added successfully" })
 
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
